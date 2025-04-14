9beb30710b63d8b947f056a343dbe0b5169f4075
Emerson Junior <89054673+emersonjr1@users.noreply.github.com>
2025-04-14 16:56:35 -0300

    Delete exercicio6.py

--- exercicio6.py
+++
@@ -1,9 +0,0 @@
-numeroInt = int(input("Digite um numero inteiro\n"))
-
-print("O numero inteiro digitado foi:",numeroInt)
-numeroAnt = numeroInt - 1
-print("O antecessor do numero é ",numeroAnt)
-
-numeroSuc = numeroInt + 1
-print("O numero sucessor é ",numeroSuc)
-
